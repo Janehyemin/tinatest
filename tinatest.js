8d7d97f0495df8c90f07a9bd35067261fd0c136d
Janehyemin <dya224@naver.com>
2019-01-18 10:32:49 +0900

    remove error

--- tinatest.js
+++ tinatest.js
@@ -20,27 +20,7 @@ apiRouter.post('/tinatest', function(req, res) {
       outputs: [
         {
           simpleText: {
-            text: "hello I'm Ryan"
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
-});
-
-apiRouter.post('/tinatest', function(req, res) {
-  console.log(req.body);
-
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleImage: {
-            imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-            altText: "hello I'm Ryan"
+            text: "티나는 힝입니다"
           }
         }
       ]
